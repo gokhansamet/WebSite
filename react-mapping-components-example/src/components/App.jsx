fd61c00962f4405d28a5e17566d78ae985d0ab7b
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-01-06 23:04:41 +0300

    Map function is converted to the arrow function

--- react-mapping-components-example/src/components/App.jsx
+++ react-mapping-components-example/src/components/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
-function EntryDetails(details) {
-  return (
-    <Entry
-      key={details.id}
-      emoji={details.emoji}
-      name={details.name}
-      description={details.meaning}
-    />
-  );
-}
+
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(EntryDetails)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((details) => (
+          <Entry
+            key={details.id}
+            emoji={details.emoji}
+            name={details.name}
+            description={details.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
