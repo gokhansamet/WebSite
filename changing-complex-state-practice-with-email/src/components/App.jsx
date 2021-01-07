1a63c419482714ad675d57257c60d401e5c32d57
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-01-08 01:09:46 +0300

    Complex State Practice With Email was added

---
+++ changing-complex-state-practice-with-email/src/components/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChanged(event) {
+    const { name, value } = event.target;
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return { fName: value, lName: prevValue.lName, email: prevValue.email };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.name,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return { fname: prevValue.name, lName: prevValue.lName, email: value };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          value={contact.fName}
+          onChange={handleChanged}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          value={contact.lName}
+          onChange={handleChanged}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input
+          value={contact.email}
+          onChange={handleChanged}
+          name="email"
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
