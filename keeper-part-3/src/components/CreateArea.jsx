ae9ce81e53b21a66d1afccf8e9ac7cb32f6ce4a8
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-01-08 13:53:44 +0300

    Part-3 was added

---
+++ keeper-part-3/src/components/CreateArea.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          value={note.title}
+          placeholder="Title"
+          onChange={handleChange}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
