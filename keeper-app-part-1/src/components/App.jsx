d2f5af17c534b64e7d3def5535a76d1f64735603
Gökhan Samet Albayrak <gokhanalbayrak43@gmail.com>
2021-02-02 12:18:23 +0300

    Keeper Part-1 was added

---
+++ keeper-app-part-1/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ keeper-app-part-1/src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a title</h1>
+      <p>This is a content</p>
+    </div>
+  );
+}
+export default Note;
